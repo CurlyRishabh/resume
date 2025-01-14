7eeb5becddfb6b9ba5c4666b33fbc1597af03152
CurlyRishabh <rissag940@gmail.com>
2025-01-14 21:59:51 +0530

    changed css for experience section

--- app/home/components/Experience.jsx
+++ app/home/components/Experience.jsx
@@ -7,9 +7,9 @@ export default function Experience() {
                 <h2 className="text-3xl font-bold mb-12 text-center">Experience</h2>
                 <div className="relative">
                     {/* Timeline line */}
-                    <div className="absolute left-1/2 transform -translate-x-1/2 w-0.5 h-full bg-gray-200" />
+                    <div className="absolute left-1/2 transform -translate-x-1/2 w-0.5 h-full bg-gray-100" />
 
-                    <div className="space-y-12">
+                    <div className="space-y-12 py-10">
                         {experienceData.map((experience, index) => (
                             <div
                                 key={index}
@@ -17,21 +17,28 @@ export default function Experience() {
                                     } items-center`}
                             >
                                 <div className={`w-1/2 ${index % 2 === 0 ? 'pr-8' : "pl-8"}`}>
-                                    <div className={`${index % 2 === 0 ? "text-right" : "text-left"}`}>
-                                        <h3 className="text-xl font-bold">{experience.company.name}</h3>
-                                        <p className="text-accent">{experience.role[0].title}</p>
-                                        <p className="text-base text-accent2">
-                                            {new Date(experience.employment_period.start_date).toLocaleDateString()} -
-                                            {experience.employment_period.end_date.length == 0 ? " Present" : new Date(experience.employment_period.end_date).toLocaleDateString()}
-                                        </p>
-                                        <ul className="mt-2 text-sm text-left space-y-3">
-                                            {experience.tasks.map((task, i) => (
-                                                <li key={i}>{task}</li>
-                                            ))}
-                                        </ul>
+                                    <div className="bg-[#173555] p-5 rounded-t-lg border-b-2 rounded-b-none">
+                                        <div className={`${index % 2 === 0 ? "text-right" : "text-left"}`}>
+                                            <div className="leading-2">
+                                                <h3 className="text-2xl font-semibold text-[#75fac8]">{experience.company.name}</h3>
+                                                <p className="text-[#f1f1e6] text-base font-semibold">{experience.role[0].title}</p>
+                                                <p className="text-base text-[#f1f1e6] italic">
+                                                    {new Date(experience.employment_period.start_date).toLocaleDateString()} -
+                                                    {experience.employment_period.end_date.length == 0 ? " Present" : new Date(experience.employment_period.end_date).toLocaleDateString()}
+                                                </p>
+                                            </div>
+                                            <ul className="mt-2 text-base text-left space-y-3 list-disc pl-5">
+                                                {experience.tasks.map((task, i) => (
+                                                    <li key={i}>{task}</li>
+                                                ))}
+                                            </ul>
+                                        </div>
                                     </div>
+
                                 </div>
-                                <div className="w-4 h-4 bg-accent rounded-full absolute left-1/2 transform -translate-x-1/2" />
+                                <div className="w-10 h-10 bg-accent rounded-full absolute left-1/2 transform -translate-x-1/2 self-start mt-4 flex p-1 items-center justify-center" >
+                                    <div className="h-full w-full bg-black rounded-full" />
+                                </div>
                                 <div className="w-1/2 pl-8" />
                             </div>
                         ))}
